0f18ac2366e0194d95aeeba19fc75a476e88086f
REPPL <REPPL@proton.me>
2023-03-09 15:12:46 +0000

    Category label added

--- source/CSVtoQDC/app.py
+++ source/CSVtoQDC/app.py
@@ -14,23 +14,44 @@ class Code:
     def generate_uuid(self):
         """Generates a random unique identifier.
         """
+
         return str(uuid.uuid4())
 
-    def __init__(self, name, colour=None, isCodable=True,
+    def __init__(self, name, parent=None, colour=None, isCodable=True,
                  isCategory=False, description=None):
         self.guid = self.generate_uuid()
-        self.name = name
-        self._colour = colour
-        self._isCodable = isCodable
-        self._isCategory = isCategory
-        self._description = description
+        self.__name = name
+        self.__colour = colour
+        self.__parent = parent
+        self.__isCodable = isCodable
+        self.__isCategory = isCategory
+        self.__description = description
+
+    @property
+    def name(self):
+        """Returns the name for this code. If it is not a category,
+        the name includes that of the category it belongs to.
+
+        Example
+        =======
+
+        Category             - ``Code Label``
+        Code within category - ``Category label - Code name``
+        """
+
+        if self.__parent:
+            return f'{self.__parent} - {self.__name}'
+        else:
+            return self.__name
+
 
     @property
     def isCodable(self):
         """Returns 'true' or 'false' depending on whether
         this code is 'codable'.(Default == 'true')
         """
-        if self._isCodable:
+
+        if self.__isCodable:
             return 'true'
         else:
             return 'false'
@@ -40,7 +61,8 @@ class Code:
         """Returns '/' if this code is a category (i.e, collection of codes)
         or '' if it is a code. (Default == '')
         """
-        if self._isCategory:
+
+        if self.__isCategory:
             return ''
         else:
             return '/'
@@ -49,8 +71,9 @@ class Code:
     def description(self):
         """Returns the XML for description.
         """
-        if self._description:
-            return f'><Description>{self._description}</Description></Code>'
+
+        if self.__description:
+            return f'><Description>{self.__description}</Description></Code>'
         else:
             return f'{self.isCategory}>'
 
@@ -59,8 +82,9 @@ class Code:
         """Returns XML code for colour if a colour has been specified.
         Returns '' otherwise.
         """
-        if self._colour:
-            return f'color="{self._colour}" '
+
+        if self.__colour:
+            return f'color="{self.__colour}" '
         else:
             return ''
 
@@ -68,6 +92,7 @@ class Code:
     def text(self):
         """Returns XML for this code. (Incl. colour, unique ID, etc.)
         """
+
         return f"""
 <Code {self.colour}guid="{self.guid}" isCodable="{self.isCodable}" name="{self.name}"{self.description}
     """.strip()
@@ -76,6 +101,7 @@ class Code:
 class CodeClosure:
     """Requires for properly formatting XML.
     """
+
     def __init__(self):
         self.text = '</Code>'
 
@@ -99,7 +125,7 @@ class Codebook:
         '#008080', '#00FFFF', '#FF00FF', '#008000'
     ]
 
-    GENERIC = 'nodes'
+    GENERIC = 'top-level-codes'
 
     DIRECTORY = {
         'import': 'import',
@@ -107,17 +133,17 @@ class Codebook:
         'errors': 'export/errors'}
 
     def __init__(self, project_name):
-        self.error_logger = Codebook.set_error_logger(project_name)
-        projects = self.read_project_files()
+        self.error_logger = Codebook.__set_error_logger(project_name)
+        projects = self.__read_project_files()
         if project_name in projects.keys():
-            self.name = project_name
-            self._clsts = projects[self.name]
-            self._codes = self.load_clsts()
+            self.__name = project_name
+            self.__code_lists = projects[self.name]
+            self.__codes = self.__load_code_lists()
         else:
-            self.name = None
+            self.__name = None
 
     @staticmethod
-    def set_error_logger(project_name):
+    def __set_error_logger(project_name):
         """Acknowledgement:
         https://stackoverflow.com/questions/55169364/
         python-how-to-write-error-in-the-console-in-txt-file
@@ -136,9 +162,32 @@ class Codebook:
         logger.addHandler(fh)
         return logger
 
-    def read_project_files(self):
+    @property
+    def name(self):
+        """Helper method to return name of this Codebook.
+        """
+
+        return self.__name
+
+    @property
+    def codes(self):
+        """Helper method to return all codes from this Codebook.
+        """
+
+        return self.__codes
+
+    @property
+    def code_lists(self):
+        """Helper method to return all code lists associated
+        with this Codebook.
+        """
+
+        return self.__code_lists
+
+    def __read_project_files(self):
         """Reads available projects and corresponding CSV files.
         """
+
         projects = {}
         directory = f'{os.getcwd()}/{Codebook.DIRECTORY["import"]}'
         for path, p, _ in os.walk(directory):
@@ -154,21 +203,23 @@ class Codebook:
                 # Remove empty entries
                 code_list = [cl for cl in projects[project] if cl]
                 projects[project] = code_list
-        print(projects)
         for project, code_lists in projects.items():
             projects[project] = sorted(code_lists)
         return projects
 
-    def load_clsts(self):
+    def __load_code_lists(self):
         """
         """
+
         directory = f'{os.getcwd()}/{Codebook.DIRECTORY["import"]}/{self.name}'
         descriptions = {}
         codes = []
-        if Codebook.GENERIC in self._clsts:
-            temp = [lst for lst in self._clsts if lst != Codebook.GENERIC]
+        if Codebook.GENERIC in self.code_lists:
+            temp = [lst for lst in self.code_lists if lst != Codebook.GENERIC]
             with open(f'{directory}/{Codebook.GENERIC}.csv') as f:
                 for row in csv.reader(f):
+                    if not row:
+                        continue
                     try:
                         label = row[0].lower()
                         description = row[1]
@@ -179,48 +230,66 @@ class Codebook:
                     temp.append(label)
                     descriptions[label] = description
         else:
-            temp = self._clsts
+            temp = self.code_lists
         colours = cycle(Codebook.COLOURS)
         for code in sorted(temp):
-            if code in self._clsts:
+            colour = next(colours)
+            if code in self.code_lists:
                 codes.append(
-                    Code(code.title(), colour=next(colours), isCategory=True))
+                    Code(code.title(),
+                         colour=colour,
+                         isCategory=True))
                 with open(f'{directory}/{code}.csv') as f:
                     for row in csv.reader(f):
+                        if not row:
+                            continue
                         try:
                             label, description = row
                             codes.append(Code(
-                                label.title(), description=description))
+                                label.title(),
+                                colour=colour,
+                                parent=code.title(),
+                                description=description))
                         except ValueError as e:
                             self.error_logger.exception(e)
                             codes.append(
-                                Code(row[0].title(), colour='#C0C0C0'))
+                                Code(row[0].title(),
+                                     colour=colour,
+                                     parent=code.title()))
                 codes.append(CodeClosure())
-            else:
-                codes.append(
-                    Code(code.title(), description=descriptions[code]))
+            # else:
+            #     codes.append(
+            #         Code(code.title(),
+            #              parent=code.title(),
+            #              description=descriptions[code]))
         return codes
 
     @property
-    def codes(self):
+    def xml(self):
         """Returns text string with XML for all codes
         in this codebook.
         """
-        return ''.join(code.text for code in self._codes)
+
+        return ''.join(code.text for code in self.codes)
 
     @property
     def text(self):
         """Returns text string with XML for this codebook.
         (This includes header and footer text.)
         """
-        return f'{Codebook.HEADER}{self.codes}{Codebook.FOOTER}'
+
+        return f'{Codebook.HEADER}{self.xml}{Codebook.FOOTER}'
 
     def __str__(self):
+        """
+        """
+
         return self.text
 
     def write(self):
         """Writes codebook as QDC to export directory.
         """
+
         if not self.name:
             return False
         directory = f'{os.getcwd()}/{Codebook.DIRECTORY["export"]}/{self.name}'
@@ -236,9 +305,5 @@ class Codebook:
             self.error_logger.exception(e)
 
 
-def main():
-    pass
-
-
 if __name__ == '__main__':
-    main()
+    pass
